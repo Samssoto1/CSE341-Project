69b93e6c497fa50e26e7e2a045eea6219adda2b7
Samssoto1 <Samssoto1@gmail.com>
2021-09-26 00:28:07 -0800

    e_com_node_app

---
+++ e_com_node_app/app.js
@@ -0,0 +1,30 @@
+// Necessities
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+// Temp engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+// Routes
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+
+
+
+
+app.use((req, res, next) => {
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
--- routes/ta02.js
+++ routes/ta02.js
@@ -1,15 +1,39 @@
 //TA02 PLACEHOLDER
 // Remember, you can make more of these placeholders yourself!
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = express.Router();
 
+let names = ["Sam", "Steven"]
+
 router.get('/', (req, res, next) => {
-  res.render('pages', {
+  res.render('pages/ta02', {
     title: 'Team Activity 02',
+    names: names,
     path: '/ta02', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
   });
 });
+
+router.post('/addUser', (req, res, next) => {
+  const user = req.body['add-user'];
+  console.log(user);
+  names.push(user);
+  res.redirect('/ta02');
+});
+
+router.post('/removeUser', (req, res, next) => {
+  const user = req.body['user'];
+  console.log(user);
+  
+  if(names.indexOf(user) != -1){
+    names.splice(names.indexOf(user), 1);
+  }
+
+  res.redirect('/ta02');
+});
+
+
 
 module.exports = router;
